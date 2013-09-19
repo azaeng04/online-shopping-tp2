99bd796e0d748e8a80929cf8c2d88add7b5030dc
nashen.naidoo <nashen.naidoo@gmail.com>
2013-09-19 18:30:45 +0000

    All test Cases complete except for the Admin.

--- Commerce Express/test/com/commerce/express/test/CRUD/FAQTest.java
+++ Commerce Express/test/com/commerce/express/test/CRUD/FAQTest.java
@@ -4,13 +4,20 @@
  */
 package com.commerce.express.test.CRUD;
 
+import com.commerce.express.app.factory.FAQFactory;
+import com.commerce.express.domain.FAQ;
 import com.commerce.express.service.crud.FAQCrudService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 /**
  *
@@ -19,6 +26,7 @@ import org.testng.annotations.BeforeMethod;
 public class FAQTest {
 
     private static ApplicationContext ctx;
+    private Long id;
     private FAQCrudService FAQCrudService;
 
     public FAQTest() {
@@ -44,5 +52,55 @@ public class FAQTest {
 
     @AfterMethod
     public void tearDownMethod() throws Exception {
+    }
+
+    @Test
+    public void createFAQ() {
+        Map<String, String> values = new HashMap<String, String>();
+        values.put("question", "How Can I Pay?");
+        values.put("answer", "By Eft");
+        FAQ faq = FAQFactory.getFAQ(values);
+
+        FAQCrudService = (FAQCrudService) ctx.getBean("FAQCrudService");
+        FAQCrudService.persist(faq);
+        id = faq.getId();
+
+        Assert.assertNotNull(faq);
+    }
+
+    @Test(dependsOnMethods = "createFAQ")
+    public void readFAQ() {
+        FAQCrudService = (FAQCrudService) ctx.getBean("FAQCrudService");
+        FAQ faq = FAQCrudService.findById(id);
+
+        Assert.assertNotNull(faq);
+    }
+
+    @Test(dependsOnMethods = "readFAQ")
+    public void updateFAQ() {
+        FAQCrudService = (FAQCrudService) ctx.getBean("FAQCrudService");
+        FAQ faq = FAQCrudService.findById(id);
+        faq.setQuestion("Where is the bussiness?");
+        FAQCrudService.merge(faq);
+
+        FAQ update = FAQCrudService.findById(id);
+        Assert.assertEquals(update.getQuestion(), "Where is the bussiness?");
+    }
+
+    @Test(dependsOnMethods = "updateFAQ")
+    public void readFAQS() {
+        FAQCrudService = (FAQCrudService) ctx.getBean("FAQCrudService");
+        List<FAQ> faq = FAQCrudService.findAll();
+
+        Assert.assertTrue(faq.size() > 0);
+    }
+
+    @Test(dependsOnMethods = "readFAQS")
+    public void deleteFAQ() {
+        FAQCrudService = (FAQCrudService) ctx.getBean("FAQCrudService");
+        FAQ faq = FAQCrudService.findById(id);
+        FAQCrudService.remove(faq);
+        FAQ deleted = FAQCrudService.findById(id);
+        Assert.assertNull(deleted);
     }
 }
--- Commerce Express/test/com/commerce/express/test/CRUD/WishListTest.java
+++ Commerce Express/test/com/commerce/express/test/CRUD/WishListTest.java
@@ -4,13 +4,27 @@
  */
 package com.commerce.express.test.CRUD;
 
+import com.commerce.express.app.factory.ProductFactory;
+import com.commerce.express.app.factory.ProductStatusFactory;
+import com.commerce.express.app.factory.WishListFactory;
+import com.commerce.express.domain.Product;
+import com.commerce.express.domain.ProductStatus;
+import com.commerce.express.domain.WishList;
 import com.commerce.express.service.crud.WishListCrudService;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.joda.time.DateTime;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 /**
  *
@@ -20,6 +34,7 @@ public class WishListTest {
 
     private static ApplicationContext ctx;
     private WishListCrudService WishListCrudService;
+    private Long id;
 
     public WishListTest() {
     }
@@ -44,5 +59,69 @@ public class WishListTest {
 
     @AfterMethod
     public void tearDownMethod() throws Exception {
+    }
+
+    @Test
+    public void createWishList() {
+
+        Map<String, String> valuesProduct = new HashMap<String, String>();
+        valuesProduct.put("productName", "Milk");
+        valuesProduct.put("description", "On Special");
+        valuesProduct.put("imageURL", "www.google.com");
+
+        ProductStatus productStatus = ProductStatusFactory.getProductStatus("InStock", 100);
+
+        Product product = ProductFactory.getProduct(valuesProduct, productStatus);
+        List<Product> productList = new ArrayList<Product>();
+        productList.add(product);
+
+        Date dateCreated = new DateTime(2012, 12, 26, 0, 0).toDate();
+        Date dateModified = new DateTime(2012, 12, 27, 0, 0).toDate();
+
+        WishList wishList = WishListFactory.getWishList(dateCreated, dateModified, 100, productList);
+
+        WishListCrudService = (WishListCrudService) ctx.getBean("WishListCrudService");
+        WishListCrudService.persist(wishList);
+        id = wishList.getId();
+
+        Assert.assertNotNull(wishList);
+    }
+
+    @Test(dependsOnMethods = "createWishList")
+    public void readWishList() {
+        WishListCrudService = (WishListCrudService) ctx.getBean("WishListCrudService");
+        WishList wishList = WishListCrudService.findById(id);
+
+        Assert.assertNotNull(wishList);
+    }
+
+    @Test(dependsOnMethods = "readWishList")
+    public void updateWishList() {
+        WishListCrudService = (WishListCrudService) ctx.getBean("WishListCrudService");
+        WishList wishList = WishListCrudService.findById(id);
+        Date dateCreated = new DateTime(2012, 11, 26, 0, 0).toDate();
+        wishList.setDateCreated(dateCreated);
+        WishListCrudService.merge(wishList);
+
+        WishList update = WishListCrudService.findById(id);
+        
+        Assert.assertEquals(update.getDateCreated(), dateCreated);
+    }
+
+    @Test(dependsOnMethods = "updateWishList")
+    public void readWishListS() {
+        WishListCrudService = (WishListCrudService) ctx.getBean("WishListCrudService");
+        List<WishList> wishList = WishListCrudService.findAll();
+
+        Assert.assertTrue(wishList.size() > 0);
+    }
+
+    @Test(dependsOnMethods = "readWishListS")
+    public void deleteWishList() {
+        WishListCrudService = (WishListCrudService) ctx.getBean("WishListCrudService");
+        WishList faq = WishListCrudService.findById(id);
+        WishListCrudService.remove(faq);
+        WishList deleted = WishListCrudService.findById(id);
+        Assert.assertNull(deleted);
     }
 }
