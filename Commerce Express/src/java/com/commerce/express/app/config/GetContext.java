df0f3cb3b062ed41ae2788c44a82e888f3a4ca2d
m04sengy@gmail.com <m04sengy@gmail.com@c6ba0f7f-4021-0742-3d08-b4883a63fdb4>
2013-10-14 11:49:51 +0000

    GetContext changed to context used

--- Commerce Express/src/java/com/commerce/express/app/config/GetContext.java
+++ Commerce Express/src/java/com/commerce/express/app/config/GetContext.java
@@ -6,13 +6,14 @@
 package com.commerce.express.app.config;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
  * @author boniface
  */
 public class GetContext {
-private static ApplicationContext ctx;
+private static ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:com/commerce/express/app/config/applicationContext-*.xml");;
 
     public static void setApplicationContext(ApplicationContext applicationContext) {
         ctx = applicationContext;
