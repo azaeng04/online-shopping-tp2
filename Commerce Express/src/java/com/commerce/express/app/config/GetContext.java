3887c0ad3aa1d921415b08ecbd690aff2ba138c4
m04sengy@gmail.com <m04sengy@gmail.com@c6ba0f7f-4021-0742-3d08-b4883a63fdb4>
2015-03-19 07:34:44 +0000

    Commerce Express clean up

--- Commerce Express/src/java/com/commerce/express/app/config/GetContext.java
+++ Commerce Express/src/java/com/commerce/express/app/config/GetContext.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.commerce.express.app.config;
 
 import org.springframework.context.ApplicationContext;
@@ -13,11 +12,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author boniface
  */
 public class GetContext {
-private static ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:com/commerce/express/app/config/applicationContext-*.xml");;
+
+    private static ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:com/commerce/express/app/config/applicationContext-*.xml");
 
     public static void setApplicationContext(ApplicationContext applicationContext) {
         ctx = applicationContext;
     }
+
     public static ApplicationContext getApplicationContext() {
         return ctx;
     }
